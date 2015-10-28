c7cf19b917c8ac17c546b3d6320f875819c606ec
i'm kamil <qoolbio@gmail.com>
2015-10-28 15:18:10 +0100

    Update get500images.sh

--- get500images.sh
+++ get500images.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl 'https://API_KEY:API_SECRET@api.cloudinary.com/v1_1/CLOUD_NAME/resources/image?max_results=500' >> data.json
+curl 'https://API_KEY:API_SECRET@api.cloudinary.com/v1_1/CLOUD_NAME/resources/image?max_results=500' > data.json
